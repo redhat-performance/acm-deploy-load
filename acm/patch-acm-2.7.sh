947fe7b8a3c7a01682b3747fd7d23c7714ad4206
Jorge Padilla <jpadilla@redhat.com>
2023-02-08 14:11:46 -0500

    reduce indexer memory limit

--- acm/patch-acm-2.7.sh
+++ acm/patch-acm-2.7.sh
@@ -38,7 +38,7 @@ oc patch search -n open-cluster-management search-v2-operator --type json -p '[{
 echo "Sleep 10"
 sleep 10
 echo "Applying ACM search-v2-operator indexer resources bump"
-oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "8Gi"}, "requests": {"memory": "128Mi", "cpu": "25m"}}}]'
+oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "4Gi"}, "requests": {"memory": "128Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
 sleep 10
 
